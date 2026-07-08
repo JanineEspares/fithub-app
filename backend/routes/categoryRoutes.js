24c762bc19042a4c3b93b62f124ff8aa75d69dfd
Janine Espares <janineespares001@gmail.com>
2026-07-08 11:17:18 +0800

    MP2 category CRUD route wiring

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const categoryController = require('../controllers/categoryController');
+const categoryAdminController = require('../controllers/categoryAdminController');
 
 const authenticate = require('../middleware/authMiddleware');
 const authorizeAdmin = require('../middleware/adminMiddleware');
@@ -17,6 +18,20 @@ router.post(
     authenticate,
     authorizeAdmin,
     categoryController.createCategory
+);
+
+router.put(
+    '/:id',
+    authenticate,
+    authorizeAdmin,
+    categoryAdminController.update
+);
+
+router.delete(
+    '/:id',
+    authenticate,
+    authorizeAdmin,
+    categoryAdminController.remove
 );
 
 module.exports = router;
